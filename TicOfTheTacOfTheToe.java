55d9f343fef390cead8ed4c09d073eec08607c1d
GrantK <grantklees108@gmail.com>
2018-02-09 13:16:50 -0800

    Finished Tic Tac Toe Game WOOT

--- TicOfTheTacOfTheToe.java
+++ TicOfTheTacOfTheToe.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Date;
 /**
  * Write a description of class TicOfTheTacOfTheToe here.
  * 
@@ -9,10 +10,11 @@ public class TicOfTheTacOfTheToe
 {
     // instance variables - replace the example below with your own
     private int x;
-    private char[] board = new char[9];
+    private String[] board = new String[9];
     private int players = 1;
     private int turns = 0;
-    private int[][] winLines = new int[][]{{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,7}, {0,4,8}, {2,4,8}};
+    private int[][] winLines = new int[][]{{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,8}};
+    private boolean playAgain = true;
     /**
      * Constructor for objects of class TicOfTheTacOfTheToe
      */
@@ -21,32 +23,36 @@ public class TicOfTheTacOfTheToe
         TicOfTheTacOfTheToe game = new TicOfTheTacOfTheToe();
         game.TicOfTheTacOfTheToe();
     }
-    
+
     public void TicOfTheTacOfTheToe()
     {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter 1 for 1 player, or Enter 2 for 2 players");
-        while(scan.hasNextInt() == false)
+        while(playAgain = true)
         {
-            scan.next();
-            System.out.println("Invalid Input!");
-            System.out.println("Enter 1 for 1 player or Enter 2 for 2 players");
-        }
-        players = scan.nextInt();
-        reset();
-        if(players == 1)
-        {
-            System.out.print("One Player Game");
-            onePlayerGame();
-        }
-        else if(players == 2)
-        {
-            System.out.print("Two Player Game");
-        }
-        else
-        {
-            System.out.println("Invalid amount of players! Please Try Again!");
-            TicOfTheTacOfTheToe();
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter 1 for 1 player, or Enter 2 for 2 players");
+            while(scan.hasNextInt() == false)
+            {
+                scan.next();
+                System.out.println("Invalid Input!");
+                System.out.println("Enter 1 for 1 player or Enter 2 for 2 players");
+            }
+            players = scan.nextInt();
+            reset();
+            if(players == 1)
+            {
+                System.out.print('\u000C');
+                onePlayerGame();
+            }
+            else if(players == 2)
+            {
+                System.out.print('\u000C');
+                twoPlayerGame();
+            }
+            else
+            {
+                System.out.println("Invalid amount of players! Please Try Again!");
+                TicOfTheTacOfTheToe();
+            }
         }
     }
 
@@ -60,57 +66,64 @@ public class TicOfTheTacOfTheToe
     {
         for(int i = 0; i < board.length; i++)
         {
-            board[i] = (char)i;
+            board[i] = Integer.toString(i);
         }
     }
-    
+
     public void show()
     {
         for(int i = 0; i < board.length; i += 3)
         {
-            System.out.println("+---+---+");
+            System.out.println("+---+---+---+");
             System.out.println("| " + board[i] + " | " + board[i+1] + " | " + board[i+2] + " |");
         }
     }
-    
+
     public boolean hasWinner()
     {
         boolean isWinner = false;
-        if(turns >= 5)
+        if(turns >= 4)
         {
             for(int i = 0; i < 8; i++)
             {
-                if(board[winLines[i][0]] == 'X' && board[winLines[i][1]] == 'X' && board[winLines[i][2]] == 'X')
+                if(board[winLines[i][0]] == "X" && board[winLines[i][1]] == "X" && board[winLines[i][2]] == "X")
                 {
                     System.out.println("Player 1 (X) Wins!");
                     isWinner = true;
                 }
-                else if(board[winLines[i][0]] == 'O' && board[winLines[i][1]] == 'O' && board[winLines[i][2]] == 'O')
+                else if(board[winLines[i][0]] == "O" && board[winLines[i][1]] == "O" && board[winLines[i][2]] == "O")
                 {
-                    System.out.println("Player 1 (X) Wins!");
+                    System.out.println("Player 2 (O) Wins!");
                     isWinner = true;
                 }
             }
         }
         return isWinner;
     }
-    
+
     public void input()
     {
         Scanner scan = new Scanner(System.in);
+        int moveIndex = 0;
+        boolean hadNextInt = false;
         if(turns % 2 == 0)
         {
             System.out.println("It is Player 1's (X) Turn.");
             System.out.println("Please Enter An Avaliable Poistion to Make Your Move.");
             show();
-             while(scan.hasNextInt() == false || board[scan.nextInt()] == 'X' || board[scan.nextInt()] == 'O')
-            {
-                System.out.println("Invalid Input!\n Please Try Again Make Sure You Enter The Number Of The Position to Make Your Move!");
-                System.out.println("It is Player 1's (X) Turn.");
-                System.out.println("Please Enter An Avaliable Poistion to Make Your Move.");
-                show();
-            }
-            board[scan.nextInt()] = 'X';
+            if(scan.hasNextInt())
+                moveIndex = scan.nextInt();
+            hadNextInt = true;
+            if(hadNextInt == false && (scan.hasNextInt() == false || board[moveIndex] == "X" || board[moveIndex] == "O"))
+                while(scan.hasNextInt() == false && hadNextInt == false)
+                {
+
+                    System.out.println("Invalid Input!\n Please Try Again Make Sure You Enter The Number Of The Position to Make Your Move!");
+                    System.out.println("It is Player 1's (X) Turn.");
+                    System.out.println("Please Enter An Avaliable Poistion to Make Your Move.");
+                    show();
+                }
+            board[moveIndex] = "X";
             turns++;
         }
         else
@@ -118,49 +131,109 @@ public class TicOfTheTacOfTheToe
             System.out.println("It is Player 2's (O) Turn.");
             System.out.println("Please Enter An Avaliable Poistion to Make Your Move.");
             show();
-            while(scan.hasNextInt() == false || board[scan.nextInt()] == 'X' || board[scan.nextInt()] == 'O')
+            while(scan.hasNextInt() == false)
             {
+                moveIndex = scan.nextInt();
                 System.out.println("Invalid Input!\n Please Try Again Make Sure You Enter The Number Of The Position to Make Your Move!");
                 System.out.println("It is Player 2's (O) Turn.");
                 System.out.println("Please Enter An Avaliable Poistion to Make Your Move.");
                 show();
+                if(board[moveIndex] != "X" || board[moveIndex] != "O")
+                    break;
             }
-            board[scan.nextInt()] = 'O';
+            board[moveIndex] = "X";
             turns++;
         }
     }
-    
+
     public void onePlayerGame()
     {
-        System.out.println("One Player Game (Player VS. Computer");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("One Player Game (Player VS. Computer)");
         while(turns < 9 && hasWinner() == false)
         {
             if(turns % 2 == 0)
             {
                 input();
+                System.out.print('\u000C');
                 System.out.println("A move was made the board now looks like this.");
                 show();
             }
             else
             {
                 int move = (int)Math.random() * 8;
-                while(board[move] == 'X' || board[move] == 'O')
+                while(board[move] == "X" || board[move] == "O")
                 {
-                    move = (int)Math.random() * 8;
+                    move = (int)(Math.random() * 8);
                 }
-                board[move] = 'O';
+                turns++;
+                board[move] = "O";
                 System.out.println("A move was made the board now looks like this.");
                 show();
             }
         }
-        if(hasWinner() == true)
+        if(turns >= 9)
+            System.out.println("The Game Was a Tie!");
+        System.out.println("Would You Like to Play Again? (Y) (N)");
+        String cont = scan.next();
+        while((cont.toLowerCase().equals("n")) == false && (cont.toLowerCase().equals("y")) == false)
         {
+            System.out.println("Invalid Input!");
             System.out.println("Would You Like to Play Again? (Y) (N)");
+            cont = scan.next();
+        }
+        if(cont.toLowerCase().equals("y"))
+        {
+            System.out.println("Thanks For Playing!\nGame will now restart!");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print('\u000C');
         }
         else
         {
+            System.out.println("Thanks For Playing!\nProgram will now exit!");
+            playAgain = false;
+        }
+    }
+
+    public void twoPlayerGame()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Two Player Game (Player VS. Player");
+        while(turns < 9 && hasWinner() == false)
+        {
+            input();
+            System.out.print('\u000C');
+            System.out.println("A move was made the board now looks like this.");
+            show();
+        }
+        if(turns >= 9)
             System.out.println("The Game Was a Tie!");
+        System.out.println("Would You Like to Play Again? (Y) (N)");
+        String cont = scan.next();
+        while((cont.toLowerCase().equals("n")) == false && (cont.toLowerCase().equals("y")) == false)
+        {
+            System.out.println("Invalid Input!");
             System.out.println("Would You Like to Play Again? (Y) (N)");
+
+        }
+        if(cont.toLowerCase().equals("y"))
+        {
+            System.out.println("Thanks For Playing!\nGame will now restart!");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print('\u000C');
+        }
+        else
+        {
+            System.out.println("Thanks For Playing!\nProgram will now exit!");
+            playAgain = false;
         }
     }
 }
